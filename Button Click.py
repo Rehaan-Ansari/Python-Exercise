5a974080b81443644f17c129b61825d35277a962
Rehaan Ansari <21336@students.mrgs.school.nz>
2024-02-28 14:39:42 +1300

    Initial Commit

---
+++ Button Click.py
@@ -0,0 +1,14 @@
+from tkinter import *
+def btn_click():
+    print("Button clicked")
+
+
+
+window = Tk()
+btn = Button(window, text="Click me!",
+command=btn_click)
+
+btn.pack()
+
+
+window.mainloop()
