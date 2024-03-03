541791faf4023dfff219358fff16d9e633175c91
Rehaan Ansari <21336@students.mrgs.school.nz>
2024-03-04 10:49:40 +1300

    Started Created Quiz

--- GUI/Color Changer.py
+++ GUI/Color Changer.py
@@ -1,6 +1,7 @@
 from tkinter import *
+import tkinter
 def color_change(color):
     main_window['background'] = color
     
-main_window = tk.TK()
+main_window = tkinter.TK()
 main_window.title("Color Changer")
