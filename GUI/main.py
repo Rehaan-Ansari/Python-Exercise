06c7268cedb785bc85b05f00ca12cab6e9b58178
Rehaan Ansari <21336@students.mrgs.school.nz>
2024-03-08 12:34:47 +1300

    Created an application that shows the students grades

--- GUI/main.py
+++ GUI/main.py
@@ -1,31 +1,48 @@
 from tkinter import *
 
-class Student():    
+class Student:
     def __init__(self, name):
-        self.first_name = name
-        
-    def set_grade(self, grade):
-        self.grade = grade
-        
-    def get_grade(self):
-        return self.grade
-    
-def show_grade():
-    grade_label.config(text=csc_2[0].get_grade())
-    pass
+        self.name = name
+        self.grades = {} 
+
+    def get_grades(self):
+        return self.grades
+
+    def set_grade(self, subject, grade):
+        self.grades[subject] = grade
+
+def show_grades(event):
+    selected_index = name_listbox.curselection()
+    if selected_index:
+        selected_student = csc_2[selected_index[0]]
+        grades = selected_student.get_grades()
+        grade_label.config(text=f"Grades for {selected_student.name}:\n{grades}")
 
 csc_2 = []
 
-csc_2.append(Student("Tharoon"))
-csc_2[0].set_grade("Not Achieved")
+student1 = Student("Amon")
+student1.set_grade("CMS", "Achieved")
 
-window =Tk()
+student2 = Student("Bilal")
+student2.set_grade("CMS", "Merit")
+
+student3 = Student("Tharin")
+student3.set_grade("CMS", "Excellence")
+
+csc_2.extend([student1, student2, student3])
+
+window = Tk()
 window.geometry("500x500")
 
-grade_label = Label()
+name_listbox = Listbox(window)
+for student in csc_2:
+    name_listbox.insert(END, student.name)
+
+name_listbox.pack()
+
+grade_label = Label(window, justify=LEFT)
 grade_label.pack()
 
-show_grade_btn = Button(text="show grade")
-show_grade_btn.pack()
+name_listbox.bind("<Double-Button-1>", show_grades)
 
-window.mainloop*()
+window.mainloop()
