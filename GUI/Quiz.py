1e11e473992b5137797633a26f9c43d6d9e63a62
Rehaan Ansari <21336@students.mrgs.school.nz>
2024-03-12 09:53:42 +1300

    Completed one window of the quiz

--- GUI/Quiz.py
+++ GUI/Quiz.py
@@ -12,5 +12,33 @@ class QuizStarter:
         self.quiz_frame.grid()#this geometry manager organizes widgets in a table-like structure in the parent widget.
         
         #label widget for the heading
-        self.heading_label=Label(self.quiz_frame, text="NZ Road Rules", font=("TwCen Mt","18", "bold"), bg=background_color)
+        self.heading_label=Label(self.quiz_frame, text="NZ Road Rules", font=("Times New Roman","18", "bold"),bg=background_color)
         self.heading_label.grid(row=0, padx=20)
+        
+        #holds value of radio buttons
+        self.var1=IntVar()
+        
+        #label for username
+        self.user_label=Label (self.quiz_frame, text="Please enter your username below: ", font=("Tw Cen MT", "16"),bg=background_color)
+        self.user_label.grid(row=1, padx=20, pady=20)
+
+        #entry box
+        self.entry_box=Entry(self.quiz_frame)
+        self.entry_box.grid(row=2,padx=20, pady=20)
+        
+        
+        #continue button
+        self.continue_button= Button(self.quiz_frame, text="Continue", font=("Helvetica", "13", "bold"), bg="orange", command=self.name_collection) 
+        self.continue_button.grid(row=3, padx=20, pady=20)
+        
+    def name_collection(self):
+        name=self.entry_box.get()
+        names.append(name) #add name to names list declared at the beginning 
+        self.quiz_frame.destroy() #destroy the starter window
+        
+
+if __name__ =="__main__":
+    root = Tk()
+    root.title("NZ Road Rules Quiz")
+    quiz_instance= QuizStarter (root) #instantiation, making an instance of the class QuizStarter 
+    root.mainloop() #so the window doesnt dissapear
